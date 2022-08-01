3d891dded4483c36bb2e8fe1c9cb66abad43cd0a
amina-seraoui <amina.seraoui@outlook.fr>
2022-08-01 15:57:14 +0200

    :hammer: homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,9 @@
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.scss';
+import '../css/app.scss'
+import {Dropdown} from 'bootstrap'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    dropdownElementList.map(function (dropdownToggleEl) {
+        return new Dropdown(dropdownToggleEl)
+    })
+})
